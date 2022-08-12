f1d55307cfb9e9dbc8c06f725a0765b4a9f592b4
ki-zuev <62571345+ki-zuev@users.noreply.github.com>
2022-08-12 18:19:01 +0300

    Delete 2 directory

--- 2/func.h
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <vector>
-#include <fstream>
-#include <filesystem>
-#include <algorithm>
-using namespace std;
-
-vector<vector<int>> read_graph(char *filename, vector<pair<int, int>>& list, vector<int>& edgesc);
-vector<int> solve(vector<vector<int>>& graph, const vector<int>& edgesc);
